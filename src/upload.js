9c5ef146badfd2c9afb043ff110385d655ffd53e
tunogya <tunogya@qq.com>
2024-08-13 23:48:38 +0800

    update

--- src/upload.js
+++ src/upload.js
@@ -1,25 +1,43 @@
 import { CID } from 'multiformats/cid'
 import * as json from 'multiformats/codecs/json'
 import { sha256 } from 'multiformats/hashes/sha2'
+import s3Client from "./utils/s3Client.js";
+import {PutObjectCommand} from "@aws-sdk/client-s3";
 
 export const handler = async (event) => {
   try {
-    const bytes = json.encode({ hello: 'world' })
-  
-    const hash = await sha256.digest(bytes)
-    const cid = CID.create(1, json.code, hash).toV1().toString()
-    
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        cidV1: cid,
-      }),
-    };
+    const file = Buffer.from(event.body, 'base64');
+    const hash = await sha256.digest(file);
+    const cid = CID.create(1, json.code, hash).toString();
+
+    const contentType = event.headers['content-type'];
+
+    try {
+      await s3Client.send(new PutObjectCommand({
+        Bucket: "nostrbucket",
+        Key: `ipfs/${cid}`,
+        Body: file,
+        ContentType: contentType,
+      }));
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          uri: `ipfs://${cid}`
+        }),
+      };
+    } catch (e) {
+      console.log(e)
+      return {
+        statusCode: 500,
+        body: "Something went wrong.",
+      }
+    }
   } catch (e) {
       console.log(e);
       return {
         statusCode: 500,
-        body: e,
+        body: "Something went wrong.",
       }
   }
 };
